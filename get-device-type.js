09db49cace66aa3a3e09b12781fa61b21373dddb
Ilya <60040129+mrProger@users.noreply.github.com>
2023-01-02 16:59:56 +0600

    Строчки с получением user agent'а заменены на вызов функции getDeviceType()

--- get-device-type.js
+++ get-device-type.js
@@ -3,16 +3,16 @@ function getDeviceType() {
 }
 
 function isPC() {
-    let agent = window.navigator.userAgent;
+    let agent = getDeviceType();
     return agent.indexOf('Windows') != -1 || agent.indexOf('Macintosh') != -1 || agent.indexOf('Linux x86') != -1;
 }
 
 function isAndroid() {
-    let agent = window.navigator.userAgent;
+    let agent = getDeviceType();
     return agent.indexOf('Android') != -1;
 }
 
 function isIOS() {
-    let agent = window.navigator.userAgent;
+    let agent = getDeviceType();
     return agent.indexOf('iPhone') != -1 || agent.indexOf('iPad') != -1;
 }
