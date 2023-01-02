3d4e6e7a2d871cf940e5fd8e1b3c0b863cdc9547
Ilya <60040129+mrProger@users.noreply.github.com>
2023-01-02 16:54:50 +0600

    Небольшой рефакторинг

--- get-device-type.js
+++ get-device-type.js
@@ -1,23 +1,18 @@
 function getDeviceType() {
-    let agent = window.navigator.userAgent;
-
-    return agent;
+    return window.navigator.userAgent;
 }
 
 function isPC() {
     let agent = window.navigator.userAgent;
-
     return agent.indexOf('Windows') != -1 || agent.indexOf('Macintosh') != -1 || agent.indexOf('Linux x86') != -1;
 }
 
 function isAndroid() {
     let agent = window.navigator.userAgent;
-
     return agent.indexOf('Android') != -1;
 }
 
 function isIOS() {
     let agent = window.navigator.userAgent;
-
     return agent.indexOf('iPhone') != -1 || agent.indexOf('iPad') != -1;
 }
