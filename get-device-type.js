546709c1e0b42b25dca3341588e749b1146cfb53
Daniil Solodkov <62195868+yanfox777@users.noreply.github.com>
2023-01-31 16:50:09 +0800

    Update get-device-type.js
    One agent variable is declared, instead of being declared in each function.

--- get-device-type.js
+++ get-device-type.js
@@ -1,18 +1,18 @@
+
 function getDeviceType() {
     return window.navigator.userAgent;
 }
 
+let agent = getDeviceType();
+
 function isPC() {
-    let agent = getDeviceType();
     return agent.indexOf('Windows') != -1 || agent.indexOf('Macintosh') != -1 || agent.indexOf('Linux x86') != -1;
 }
 
 function isAndroid() {
-    let agent = getDeviceType();
     return agent.indexOf('Android') != -1;
 }
 
 function isIOS() {
-    let agent = getDeviceType();
     return agent.indexOf('iPhone') != -1 || agent.indexOf('iPad') != -1;
 }
