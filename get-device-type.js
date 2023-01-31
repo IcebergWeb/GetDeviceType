721de3d384d4d3c008182c4b2a243aa9313082cb
Ilya <60040129+mrProger@users.noreply.github.com>
2023-01-31 15:05:53 +0600

    Рефакторинг

--- get-device-type.js
+++ get-device-type.js
@@ -2,17 +2,16 @@ function getDeviceType() {
     return window.navigator.userAgent;
 }
 
+const agent = getDeviceType();
+
 function isPC() {
-    let agent = getDeviceType();
     return agent.indexOf('Windows') != -1 || agent.indexOf('Macintosh') != -1 || agent.indexOf('Linux x86') != -1;
 }
 
 function isAndroid() {
-    let agent = getDeviceType();
     return agent.indexOf('Android') != -1;
 }
 
 function isIOS() {
-    let agent = getDeviceType();
     return agent.indexOf('iPhone') != -1 || agent.indexOf('iPad') != -1;
 }
